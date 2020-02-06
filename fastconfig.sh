5f1567b22e575833ba5d139109e1ece036877411
username77177 <username77177@gmail.com>
2020-02-06 21:46:25 +0300

    06/02/2020 21:46:24

--- fastconfig.sh
+++ fastconfig.sh
@@ -3,6 +3,9 @@ sudo apt install fish vim tmux
 sudo add-apt-repository ppa:mmstick76/alacritty
 sudo apt install alacritty
 mkdir -p  ~/.config/alacritty
+mv ~/.config/alacritty/{alacritty.yml,alacritty.old}
+mv ~/.{vimrc,vimrc.old}
+mv ~/.{tmux.conf,tmux.conf.old}
 ln -sfr ./alacritty.yml ~/.config/alacritty/alacritty.yml
 ln -sfr ./.vimrc ~/
 ln -sfr ./.tmux.conf ~/
