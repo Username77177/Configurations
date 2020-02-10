2fe46a962d483881bade67af094dfe46bc2142d8
username77177 <username77177@gmail.com>
2020-02-10 22:48:10 +0300

    10/02/2020 22:47:31 | i3 config maybe ready

--- i3install.sh
+++ i3install.sh
@@ -10,11 +10,12 @@ sudo apt install compton feh rofi lxappearance scrot i3
 # Rofi - лаунчер программ (альтернатива dmenu)
 # lxappearance - иконки, шрифты и многое другое
 
+# Installing i3-gaps
+mkdir $HOME/gitinstalled
 sudo apt install libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf xutils-dev libtool libxcb-shape0-dev
 sudo apt-get install libxcb-xrm-dev
-# Installing i3-gaps
-cd /tmp
-git clone https://www.github.com/Airblader/i3 i3-gaps
+cd ~/gitinstalled/
+git clone https://www.github.com/Airblader/i3 ~/gitinstalled/i3-gaps
 cd i3-gaps
 git checkout gaps && git pull
 autoreconf --force --install
@@ -24,3 +25,11 @@ cd build
 ../configure --prefix=/usr --sysconfdir=/etc
 make
 sudo make install
+
+# Installing xwinwrap (Git animated wallpaper)
+sudo apt-get install xorg-dev build-essential libx11-dev x11proto-xext-dev libxrender-dev libxext-dev
+git clone https://github.com/ujjwal96/xwinwrap.git ~/gitinstalled/xwinwrap
+cd xwinwrap
+make
+sudo make install
+make clean
