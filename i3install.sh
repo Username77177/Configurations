a3c387f1601be112e4df661dd10b656bc1a7e5ca
username77177 <username77177@gmail.com>
2020-02-10 21:20:06 +0300

    10/02/2020 21:20:06

--- i3install.sh
+++ i3install.sh
@@ -4,9 +4,23 @@
 # https://i3wm.org/docs/userguide.html
 # https://wiki.archlinux.org/index.php/I3_(%D0%A0%D1%83%D1%81%D1%81%D0%BA%D0%B8%D0%B9)#%D0%9D%D0%B0%D1%81%D1%82%D1%80%D0%BE%D0%B9%D0%BA%D0%B0
 
-sudo apt install compton hsetroot rofi lxappearance scrot i3
+sudo apt install compton feh rofi lxappearance scrot i3
 # Compton - Эффекты (тени, затемнение, прозрачность и т.д.)
-# Hsetroot - выбор обоев
+# feh - выбор обоев
 # Rofi - лаунчер программ (альтернатива dmenu)
-mkdir ~/.config/i3
-ln -sfr ./i3wm/config_i3 ~/.config/i3/config
+# lxappearance - иконки, шрифты и многое другое
+
+sudo apt install libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf xutils-dev libtool libxcb-shape0-dev
+sudo apt-get install libxcb-xrm-dev
+# Installing i3-gaps
+cd /tmp
+git clone https://www.github.com/Airblader/i3 i3-gaps
+cd i3-gaps
+git checkout gaps && git pull
+autoreconf --force --install
+rm -rf build
+mkdir build
+cd build
+../configure --prefix=/usr --sysconfdir=/etc
+make
+sudo make install
